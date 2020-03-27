bf8722673b5b472abf5bb0415fb07762182831e2
stanicchris <chris574topher@gmail.com>
2020-03-27 17:05:37 +0100

    test integr

---
+++ src/main/groovy/gl51/movie/service/impl/MovieRegistryImpl.groovy
@@ -0,0 +1,24 @@
+package gl51.movie.service.impl
+
+import gl51.movie.data.Movie
+import gl51.movie.service.MovieRegistry
+
+
+import javax.inject.Singleton
+
+@Singleton
+class MovieRegistryImpl implements gl51.movie.service.MovieRegistry {
+
+    private List<Movie> internalRegistry = []
+
+
+    @Override
+    void addMovieToFavorites(String imdbID) {
+        internalRegistry << new Movie(imbdID: imbdID)
+    }
+
+    @Override
+    List<Movie> listFavorites() {
+        internalRegistry
+    }
+}
---
+++ src/test/groovy/gl51/movie/service/impl/MovieRegistryImplTest.groovy
@@ -0,0 +1,30 @@
+package gl51.movie.service.impl
+
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Specification
+
+import javax.inject.Inject
+
+@MicronautTest
+class MovieRegistryImplTest extends Specification {
+
+    @Inject
+    MovieRegistryImpl registry
+
+    void"inject should work"() {
+        expect:
+        registry != null
+    }
+
+    void"favorites should be empty"() {
+        expect:
+        registry.listFavorites() == []
+    }
+
+    void "addind a favorites should fill in the database"() {
+        when:
+        registry.addMovieToFavorites( imbdID: "aaaa")
+        then:
+        registry.listFavorites().size() == 1
+    }
+}
