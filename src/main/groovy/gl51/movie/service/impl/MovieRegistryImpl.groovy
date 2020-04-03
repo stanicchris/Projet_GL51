1d45552551ae5ad620c7cc1fd69f7c8f8c2c5e12
stanicchris <chris574topher@gmail.com>
2020-04-03 16:47:30 +0200

    create client test

--- src/main/groovy/gl51/movie/service/impl/MovieRegistryImpl.groovy
+++ src/main/groovy/gl51/movie/service/impl/MovieRegistryImpl.groovy
@@ -1,20 +1,24 @@
 package gl51.movie.service.impl
 
 import gl51.movie.data.Movie
+import gl51.movie.service.MovieClient
 import gl51.movie.service.MovieRegistry
 
-
+import javax.inject.Inject
 import javax.inject.Singleton
 
+// Shift + ctl + T => créer un test
 @Singleton
 class MovieRegistryImpl implements MovieRegistry {
 
     private List<Movie> internalRegistry = []
 
+    @Inject
+    private MovieClient movieClient
 
     @Override
     void addMovieToFavorites(String imdbID) {
-        internalRegistry << new Movie(imdbID: imdbID)
+        internalRegistry << movieClient.getMovieDetail(imdbID)
     }
 
     @Override
